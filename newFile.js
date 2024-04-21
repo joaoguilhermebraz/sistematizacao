eac0f3956836d924d03a05e6a6c924326b309bcf
joaoguilhermebraz <joaoguilhermebraz18@gmail.com>
2024-04-21 17:19:40 -0300

    git add contato-script.js

---
+++ newFile.js
@@ -0,0 +1,26 @@
+[
+    {
+        "nome": "Dr. Carlos Santos",
+        "especialidade": "Dermatologista",
+        "telefone": "(61) 9 9848-1524",
+        "email": "carlos.santos@gmail.com"
+    },
+    {
+        "nome": "Dr. João Silva",
+        "especialidade": "Cardiologia",
+        "telefone": "(61) 9 9848-1424",
+        "email": "joao.silva@gmail.com"
+    },
+    {
+        "nome": "Dra. Maria Oliveira",
+        "especialidade": "Pediatria",
+        "telefone": "(61) 9 9848-2545",
+        "email": "maria.oliveira@gmail.com"
+    },
+    {
+        "nome": "Dr. Carlos Santos",
+        "especialidade": "Ortopedia",
+        "telefone": "(61) 9 9848-1524",
+        "email": "carlos.santos@gmail.com"
+    },
+];
